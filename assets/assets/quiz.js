6b8e16f0fe38dc67e55bb06040c561d602338e3d
NataliaReyesG <natalia.arg9@gmail.com>
2021-08-10 11:37:40 -0500

    Quiz

---
+++ assets/assets/quiz.js
@@ -0,0 +1,14 @@
+function captura(){
+    let nombres=document.getElementById("nombres").value;
+    let apellidos=document.getElementById("apellidos").value;
+    let edad=document.getElementById("edad").value;
+    let sangre=document.getElementById("tipo de sangre").value;
+    let rh=document.getElementById("rh").value;
+    let telefono=document.getElementById("telefono").value;
+
+   
+        alert(nombres+" "+apellidos+" "+edad+" "+sangre+" "+rh+" "+telefono);
+
+    
+}
+    
